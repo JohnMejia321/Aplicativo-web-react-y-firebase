d38ecc5224da34c91c41ff59ef6342236e27faef
John Mejia <john.mejias@udea.edu.co>
2021-02-18 14:37:43 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import Grid from "@material-ui/core/Grid";
+import "./App.css"
+import ListaInmuebes from "./componentes/vistas/ListaInmuebes"
+import AppNavbar from "./componentes/layout/AppNavbar"
+import { BrowserRouter as Router, Switch,Route,Redirect } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import theme from "./theme/theme";
+import RegistrarUsuario from "./componentes/seguridad/RegistrarUsuario"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends  Component {
+   
+  render(){
+    return (
+       
+      <Router>
+        <div>
+      <MuiThemeProvider theme={theme}>
+        <AppNavbar />
+        <Grid container>
+          <Switch>
+         <Route exact path='/' render={() => <ListaInmuebes name="home" />} />> 
+         <Route exact path='/auth/registrarUsuario' render={() => <RegistrarUsuario name="registrarUsuario" />} />> 
+        </Switch>
+        </Grid>
+          </MuiThemeProvider>
+          </div>
+        </Router>
+         
+      
+
+        
+        
+        
+      
+    )
+  }
+
 }
 
 export default App;
+
---
+++ src/componentes/vistas/ListaInmuebes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Button from "@material-ui/core/Button";
+
+
+class ListaInmuebes extends Component {
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="primary">color primario</Button>
+                <Button variant="contained" color="secondary">color secundario</Button>
+
+
+            </div>
+        )
+    }
+}
+
+export default ListaInmuebes 
